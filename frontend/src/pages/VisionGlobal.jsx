149c77cc1f6c08aad085207285226ce01486d069
Manuel <manuel.t.shade@gmail.com>
2024-03-28 23:42:16 +0000

    feat: WIP - preparando overview

--- frontend/src/pages/VisionGlobal.jsx
+++ frontend/src/pages/VisionGlobal.jsx
@@ -1,64 +1,42 @@
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-} from 'chart.js'
-import { Line } from 'react-chartjs-2'
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-)
-
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top'
-    },
-    title: {
-      display: true,
-      text: 'Overview'
-    }
-  }
-}
+import { useEffect, useState } from 'react'
+import LineChart from '../components/LineChart'
+import usePets from '../hooks/usePets'
+import { getPetHistory } from '../services/pets.services'
+import useAuth from '../hooks/useAuth'
 
 const VisionGlobal = () => {
+  const { user } = useAuth()
+  const { pets } = usePets()
+  const [datasets, setDataSets] = useState()
+
   const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
 
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: labels.map(() => Math.random()),
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)'
-      },
-      {
-        label: 'Dataset 2',
-        data: labels.map(() => Math.random()),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)'
-      }
-    ]
-  }
+  const data = [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => Math.random()),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)'
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => Math.random()),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)'
+    }
+  ]
+
+  useEffect(() => {
+    if (pets) {
+      Promise.all(pets?.map(pet => getPetHistory({ idPet: pet.ID, jwt: user.jwt })))
+        .then((res) => console.log(res))
+    }
+  }, [pets])
 
   return (
     <div>
-      <Line
-        options={options}
-        data={data}
+      <LineChart labelsX={labels} title={'overview'}
+        datasets={data}
 />
     </div>
   )
