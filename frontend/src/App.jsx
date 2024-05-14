4e44c1610c63d1f784689f1f2318b58dca1c9ac9
Manuel <manuel.t.shade@gmail.com>
2024-05-14 12:06:16 +0100

    refactor: eliminado css no usado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,6 @@ import AddNewPet from './pages/AddNewPet'
 import PetHistory from './pages/PetHistory'
 import ProtectedRoute from './components/protectedRoute/ProtectedRoute'
 import { ROUTES } from './constants/routes'
-// import VisionGlobal from './pages/VisionGlobal'
 import UpdatePet from './pages/UpdatePet'
 
 function App () {
@@ -28,7 +27,6 @@ function App () {
           <Route path={ROUTES.PROFILE} element={<ProtectedRoute><Profile /></ProtectedRoute>} />
           <Route path={ROUTES.SINGLE_PET} element={<ProtectedRoute><PetProfile /></ProtectedRoute>} />
           <Route path={ROUTES.PET_HISTORY} element={<ProtectedRoute><PetHistory /></ProtectedRoute>} />
-          {/* <Route path={ROUTES.PETS_OVERVIEW} element={<ProtectedRoute><VisionGlobal /></ProtectedRoute>} /> */}
           <Route path={ROUTES.NEW_PET} element={<ProtectedRoute><AddNewPet /></ProtectedRoute>} />
           <Route path={ROUTES.UPDATE_PET} element={<ProtectedRoute><UpdatePet /></ProtectedRoute>} />
           <Route path={ROUTES.EXPENSES} element={<ProtectedRoute><Expenses /></ProtectedRoute>} />
