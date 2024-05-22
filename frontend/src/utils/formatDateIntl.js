46832762dfb8b797368b1bb070435fe7086700eb
Manuel <manuel.t.shade@gmail.com>
2024-05-22 14:25:44 +0100

    fix: bug fecha funcion utilidad formatDateIntl.js

--- frontend/src/utils/formatDateIntl.js
+++ frontend/src/utils/formatDateIntl.js
@@ -1,5 +1,7 @@
-export const formatDateIntl = (fecha) => new Intl.DateTimeFormat('es-ES', {
-  day: '2-digit',
-  month: '2-digit',
-  year: 'numeric'
-}).format(new Date(fecha))
+export const formatDateIntl = (fecha) => fecha
+  ? new Intl.DateTimeFormat('es-ES', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  }).format(new Date(fecha))
+  : ''
